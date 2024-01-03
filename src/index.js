1eba587fe37f9a7401f110466507569051cdec7c
DharmpreetAtwal <88208715+DharmpreetAtwal@users.noreply.github.com>
2024-01-03 13:59:16 -0500

    - Initial commit - Added svg, trasforming toSVGCoordinates

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import { Planet } from "./planets.js";
+import { useState, useRef, useEffect, createContext } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export const MyTransform = createContext();
+const MyTransformProvider = ({ children, svgRef }) => {
+  const toSVGCoordinates = (event) => {
+    const clientX = event.clientX;
+    const clientY = event.clientY;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const viewBox = svgRef.current.getAttribute("viewBox").split(" ");
+    const svgWidth = parseInt(svgRef.current.getAttribute("width"));
+    const svgHeight = parseInt(svgRef.current.getAttribute("height"));
+
+    const scaleX = parseInt(viewBox[2]) / svgWidth;
+    const scaleY = parseInt(viewBox[3]) / svgHeight;
+
+    const canvasRect = svgRef.current.getBoundingClientRect();
+    const canvasX = (clientX - canvasRect.left) * scaleX + parseInt(viewBox[0]);
+    const canvasY = (clientY - canvasRect.top) * scaleY + parseInt(viewBox[1]);
+
+    return [Math.round(canvasX), Math.round(canvasY)];
+  };
+
+  const contextValue = {
+    toSVGCoordinates,
+  };
+
+  return (
+    <MyTransform.Provider value={contextValue}>{children}</MyTransform.Provider>
+  );
+};
+
+const Map = () => {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [width, setWidth] = useState(5000);
+  const [height, setHeight] = useState(5000);
+
+  const [viewBoxX, setViewBoxX] = useState(0);
+  const [viewBoxY, setViewBoxY] = useState(0);
+  const [viewBoxWidth, setViewBoxWidth] = useState(1000);
+  const [viewBoxHeight, setViewBoxHeight] = useState(1000);
+
+  const svgRef = useRef(null);
+
+  useEffect(() => {
+    // Update the body's overflow property based on the state
+    document.body.style.overflow = "hidden";
+  }, []);
+
+  return (
+    <>
+      <MyTransformProvider svgRef={svgRef}>
+        <svg
+          ref={svgRef}
+          x={x}
+          y={y}
+          width={width}
+          height={height}
+          viewBox={`${viewBoxX} ${viewBoxY} ${viewBoxWidth} ${viewBoxHeight}`}
+        >
+          <Planet />
+        </svg>
+      </MyTransformProvider>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Map />);
---
+++ src/planets.js
@@ -0,0 +1,47 @@
+import { useContext, useRef, useState } from "react";
+import { MyTransform } from "./index.js";
+
+export const Planet = () => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [x, setX] = useState(50);
+  const [y, setY] = useState(50);
+  const [r, setR] = useState(50);
+
+  const clickX = useRef(0);
+  const clickY = useRef(0);
+
+  const { toSVGCoordinates } = useContext(MyTransform);
+
+  const handleOnDown = (event) => {
+    const coord = toSVGCoordinates(event);
+    clickX.current = coord[0] - x;
+    clickY.current = coord[1] - y;
+    setIsDragging(true);
+  };
+
+  const handleMove = (event) => {
+    if (isDragging) {
+      const coord = toSVGCoordinates(event);
+      console.log(coord);
+      setX(coord[0] - clickX.current);
+      setY(coord[1] - clickY.current);
+    }
+  };
+
+  const handleOnUp = (event) => {
+    setIsDragging(false);
+  };
+
+  return (
+    <>
+      <circle
+        cx={x}
+        cy={y}
+        r={r}
+        onMouseDown={handleOnDown}
+        onMouseMove={handleMove}
+        onMouseUp={handleOnUp}
+      />
+    </>
+  );
+};
